cb38fbeee21472f878001c4280ea2a6d3d275c21
sidna502 <74568365+sidna502@users.noreply.github.com>
2022-01-01 21:53:47 +0100

    First

--- Assignement_051_055_11.py
+++ Assignement_051_055_11.py
@@ -1,58 +1,58 @@
 # First
-# my_numbers = [15, 81, 5, 17, 20, 21, 13]
-# num2 = []
-# a = 0
-# for number in my_numbers:
+my_numbers = [15, 81, 5, 17, 20, 21, 13]
+num2 = []
+a = 0
+for number in my_numbers:
 
-#     if number % 5 == 0:
-#         num2.append(number)
-#     num2.sort(reverse=True)
+    if number % 5 == 0:
+        num2.append(number)
+    num2.sort(reverse=True)
 
-# for number in num2:
+for number in num2:
 
-#     a += 1
-#     print(f'{a} => {number}')
+    a += 1
+    print(f'{a} => {number}')
 
-# print('All numbers printed')
-# print('#' * 50)
+print('All numbers printed')
+print('#' * 50)
 
 # Second
 
-# for num in range(1, 21):
-#     if num == 6:
-#         continue
-#     elif num == 8:
-#         continue
-#     elif num == 12:
-#         continue
-#     else:
-#         print(f'{str(num).zfill(2)}')
+for num in range(1, 21):
+    if num == 6:
+        continue
+    elif num == 8:
+        continue
+    elif num == 12:
+        continue
+    else:
+        print(f'{str(num).zfill(2)}')
+Third
+total_marks = 0
+my_rank = {
+    'Math': 'A',
+    'Science': 'B',
+    'Drawing': 'A',
+    'Sport': 'C'
+}
+A = 100
+B = 80
+C = 40
+for rank_key, rank_value in my_rank.items():
+    if my_rank[rank_key] == "A":
+        print(
+            f'My rank in {rank_key} is {rank_value} and this equal {A} points')
+        total_marks += A
+    elif my_rank[rank_key] == 'B':
+        print(
+            f'My rank in {rank_key} is {rank_value} and this equal {B} points')
+        total_marks += B
+    elif my_rank[rank_key] == 'C':
+        print(
+            f'My rank in {rank_key} is {rank_value} and this equal {C} points')
+        total_marks += C
+print(f'Total points is {total_marks}')
 # Third
-# total_marks = 0
-# my_rank = {
-#     'Math': 'A',
-#     'Science': 'B',
-#     'Drawing': 'A',
-#     'Sport': 'C'
-# }
-# A = 100
-# B = 80
-# C = 40
-# for rank_key, rank_value in my_rank.items():
-#     if my_rank[rank_key] == "A":
-#         print(
-#             f'My rank in {rank_key} is {rank_value} and this equal {A} points')
-#         total_marks += A
-#     elif my_rank[rank_key] == 'B':
-#         print(
-#             f'My rank in {rank_key} is {rank_value} and this equal {B} points')
-#         total_marks += B
-#     elif my_rank[rank_key] == 'C':
-#         print(
-#             f'My rank in {rank_key} is {rank_value} and this equal {C} points')
-#         total_marks += C
-# print(f'Total points is {total_marks}')
-# Fourth
 students = {
     "Ahmed": {
         "Math": "A",
@@ -107,26 +107,26 @@ for student_name in students:
     total_marks = 0
 
 # Fourth 2
-# for student_key, student_value in students.items():
-#     print('-' * 50)
-#     print(f'\"-- Student name => {student_key}\"')
-#     print('-' * 50)
-#     for child_student_key, child_student_value in student_value.items():
-#         if students[student_key][child_student_key] == 'A':
-#             print(f'\"- {child_student_key} => {A} points\"')
-#             total_marks += A
+for student_key, student_value in students.items():
+    print('-' * 50)
+    print(f'\"-- Student name => {student_key}\"')
+    print('-' * 50)
+    for child_student_key, child_student_value in student_value.items():
+        if students[student_key][child_student_key] == 'A':
+            print(f'\"- {child_student_key} => {A} points\"')
+            total_marks += A
 
-#         elif students[student_key][child_student_key] == 'B':
-#             print(f'\"- {child_student_key} => {B} points\"')
-#             total_marks += B
+        elif students[student_key][child_student_key] == 'B':
+            print(f'\"- {child_student_key} => {B} points\"')
+            total_marks += B
 
-#         elif students[student_key][child_student_key] == 'C':
-#             print(f'\"- {child_student_key} => {C} points\"')
-#             total_marks += C
+        elif students[student_key][child_student_key] == 'C':
+            print(f'\"- {child_student_key} => {C} points\"')
+            total_marks += C
 
-#         elif students[student_key][child_student_key] == 'D':
-#             print(f'\"- {child_student_key} => {D} points\"')
-#             total_marks += D
+        elif students[student_key][child_student_key] == 'D':
+            print(f'\"- {child_student_key} => {D} points\"')
+            total_marks += D
 
-#     print(f'Total point for {student_key} is {total_marks}')
-#     total_marks = 0
+    print(f'Total point for {student_key} is {total_marks}')
+    total_marks = 0
--- Assignement_056_059_12.py
+++ Assignement_056_059_12.py
@@ -1,42 +1,43 @@
 # First
-# def calculate(A, B, op='unknown'):
-#     if op.lower() == 'add' or op.lower() == 'a':
-#         print(A+B)
-#     elif op.lower == 'substrct' or op.lower() == 's':
-#         print(A-B)
-#     elif op.lower() == 'multiply' or op.lower() == 'm':
-#         print(A*B)
-#     elif op.lower() == 'unknown' or op.lower() == 'u':
-#         print(A+B)
+def calculate(A, B, op='unknown'):
+    if op.lower() == 'add' or op.lower() == 'a':
+        print(A+B)
+    elif op.lower == 'substrct' or op.lower() == 's':
+        print(A-B)
+    elif op.lower() == 'multiply' or op.lower() == 'm':
+        print(A*B)
+    elif op.lower() == 'unknown' or op.lower() == 'u':
+        print(A+B)
 
-#     else:
-#         print('Wrong operation')
+    else:
+        print('Wrong operation')
 
 
-# calculate(10, 20, 'aDD')
-# calculate(10, 20, 's')
-# calculate(10, 20, 'm')
-# calculate(10, 20, 'n')
-# calculate(10, 20)
+calculate(10, 20, 'aDD')
+calculate(10, 20, 's')
+calculate(10, 20, 'm')
+calculate(10, 20, 'n')
+calculate(10, 20)
 # Second
 
 
-# def addition(*numbers):
-#     n = 0
-#     for num in numbers:
-#         if num == 10:
-#             continue
-#         elif num == 5:
-#             n -= 5
+def addition(*numbers):
+    n = 0
+    for num in numbers:
+        if num == 10:
+            continue
+        elif num == 5:
+            n -= 5
 
-#         n += num
+        n += num
 
-#     print(n)
+    print(n)
 
 
-# addition(10, 50, 50, 5, 15)
+addition(10, 50, 50, 5, 15)
 
-# # Third
+# Third
+
 
 def show_skills(name, *skills):
     if len(skills) == 0:
@@ -51,9 +52,12 @@ show_skills('Sidna', 'Html', 'Css', 'Python')
 show_skills('Sidna')
 
 # Fourth
-# def say_hello(name='unknown', age='unknown', country='unknown'):
-#     print(
-#         f'Hello {name.capitalize().strip()} your age is {age} years old and your country is {country.capitalize()}')
 
 
+def say_hello(name='unknown', age='unknown', country='unknown'):
+    print(
+        f'Hello {name.capitalize().strip()} your age is {age} years old and your country is {country.capitalize()}')
+
+
+say_hello('sidna', 30, 'mauritania')
 # say_hello('sidna', 30, 'mauritania')
