ebd4cba23e5bd724f44035acbda257ffaf508bb4
jsidna <jedousidna@gmail.com>
2022-09-05 20:31:06 +0200

    done

--- Assignement_064_068_14.py
+++ Assignement_064_068_14.py
@@ -1,1 +1,19 @@
-print('start')
+import os
+# First
+num = 1
+
+while num <= 50:
+    if num == 25:
+        myFile = open("special-text.text ", 'w')
+
+    else:
+        myFile = open(f"Text{str(num)}.text ", 'w')
+        myFile.write(f"Elzero Web School => {num}")
+
+    num += 1
+    myFile.close()
+
+print(os.getcwd())      # Current Working Directory
+print(os.path.dirname(os.path.abspath(__file__)))      # The Path of My Folder
+print(os.path.basename(myFile.name))
+print(num)
